ec7fa4365a549a5c3b0daa99a281b9eed2211cf6
wanghaitao <15124505701@163.com>
2024-04-17 09:40:54 +0800

    style: format document

--- apps/admin/src/app.interceptor.ts
+++ apps/admin/src/app.interceptor.ts
@@ -1,10 +1,4 @@
-import {
-  Injectable,
-  NestInterceptor,
-  ExecutionContext,
-  CallHandler,
-  Logger,
-} from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
 import { CommonResult } from '@libs/common/api/common-result';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
